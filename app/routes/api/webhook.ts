8139e7b10945950be8a605d93cc978d28acc3434
Marco Link <marco.link@contentful.com>
2022-01-29 19:10:11 +0100

    add GET handler as well

--- app/routes/api/webhook.ts
+++ app/routes/api/webhook.ts
@@ -1,9 +1,17 @@
-import {ActionFunction} from "remix";
+import {ActionFunction, LoaderFunction} from "remix";
+
+export const loader: LoaderFunction = ({request, params}) => {
+    console.log(request, params)
+    return new Response(JSON.stringify({"method": request.method}), {
+        status: 200,
+        headers: {}
+    });
+}
 
 // POST Request
 export const action: ActionFunction = ({request, params}) => {
     console.log(request, params)
-    return new Response(null, {
+    return new Response(JSON.stringify({"method": request.method}), {
         status: 200,
         headers: {}
     });
