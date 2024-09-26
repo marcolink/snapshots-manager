bd76c0d9b77a50de7a90a79e5b21edb4cfa71cdf
Marco Link <marco.link@contentful.com>
2024-09-26 22:40:32 +0200

    feat: improve user query

--- app/hooks/useContentfulUsers.tsx
+++ app/hooks/useContentfulUsers.tsx
@@ -1,21 +1,21 @@
-import {useInBrowserSdk} from "~/hooks/useInBrowserSdk";
-import {useQuery} from "@tanstack/react-query";
+import { useInBrowserSdk } from '~/hooks/useInBrowserSdk';
+import { useQuery } from '@tanstack/react-query';
 
 export function useContentfulUsers(userIds: string[]) {
-  const {cma, sdk} = useInBrowserSdk()
+  const { cma, sdk } = useInBrowserSdk();
 
   return useQuery({
-    queryKey: [sdk?.ids, 'contentful-users', userIds],
+    queryKey: [sdk?.ids.organization, sdk?.ids.space, sdk?.ids.environment, 'users', userIds],
     queryFn: async () => {
       const result = await cma?.user.getManyForSpace({
         spaceId: sdk?.ids.space,
         organizationId: sdk?.ids.organization,
         query: {
-          'sys.id[in]': userIds
-        }
-      })
+          'sys.id[in]': userIds,
+        },
+      });
 
-      return result?.items || []
+      return result?.items || [];
     },
     enabled: !!userIds.length,
     initialData: [],
